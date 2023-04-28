2b7bb6ad3f8a6d358bcd9d123a9088852871ee3a
Mortey Kafui <morteykafui@gamil.com>
2023-04-28 06:59:38 +0000

    refactor remove duplicate intergaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,34 +1,34 @@
 import { HStack, Icon } from '@chakra-ui/react'
-import { Platform } from '../hooks/useGames'
-import {FaWindows,FaPlaystation,FaXbox,FaLinux,FaAndroid,FaApple} from  'react-icons/fa'
-import { MdPhoneIphone} from 'react-icons/md'
-import {SiNintendo} from 'react-icons/si'
-import {BsGlobe} from 'react-icons/bs'
+import { Platform } from '../hooks/usePlatforms'
+import { FaWindows, FaPlaystation, FaXbox, FaLinux, FaAndroid, FaApple } from 'react-icons/fa'
+import { MdPhoneIphone } from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import { BsGlobe } from 'react-icons/bs'
 import { IconType } from 'react-icons'
 
 
-interface Props{
-  platforms:Platform[]
+interface Props {
+  platforms: Platform[]
 }
 
-const PlatformIconList = ({platforms}:Props) => {
-  const iconMap:{[key:string]:IconType}={
-    pc:FaWindows,
-    playstation:FaPlaystation,
-    xbox:FaXbox,
-    nintendo:SiNintendo,
-    mac:FaApple,
-    linux:FaLinux,
-    ios:MdPhoneIphone,
-    web:BsGlobe,
-    android:FaAndroid
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid
 
   }
 
 
   return (
     <HStack marginY={'10px'}>
-    {platforms.map(platform=><Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />)}
+      {platforms.map(platform => <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />)}
     </HStack>
   )
 }
