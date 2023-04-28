01b5c8e43b042b803f093ac6eb75c0187174a4ae
Mortey Kafui <morteykafui@gamil.com>
2023-04-28 06:42:49 +0000

    fetch platforms with react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,21 @@
-import useData from "./useData"
+import { useQuery } from '@tanstack/react-query';
+import { FtechResponse } from "./useData"
+import apiClient from '../services/api-client';
 
-interface Platform{
-  id:number,
-  name:string,
-  slug:string
+interface Platform {
+  id: number,
+  name: string,
+  slug: string
 }
 
 
-const usePlatforms=()=>useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => useQuery({
+  queryKey: ['platforms'],
+  queryFn: () => apiClient.get<FtechResponse<Platform>>('/platforms/lists/parents').then(res => res.data), staleTime: 24 * 60 * 60 * 1000,
+  // initialData: {
+  //   count: platforms.length, results: platforms
+  // }
+})
 
 
 export default usePlatforms
